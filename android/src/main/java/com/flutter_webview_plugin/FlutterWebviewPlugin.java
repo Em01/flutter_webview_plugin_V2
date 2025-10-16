e96aa65235fb9d78525b943b0a32758c001338b1
Emma Williams <github@em01.co.uk>
2025-10-16 12:58:55 +0100

    update for flutter version

--- android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
+++ android/src/main/java/com/flutter_webview_plugin/FlutterWebviewPlugin.java
@@ -36,7 +36,7 @@ public class FlutterWebviewPlugin implements FlutterPlugin, ActivityAware, Metho
     private static final String CHANNEL_NAME = "flutter_webview_plugin";
     private static final String JS_CHANNEL_NAMES_FIELD = "javascriptChannelNames";
 
-    public static void registerWith(PluginRegistry.Registrar registrar) {
+    public static void registerWith(io.flutter.plugin.common.PluginRegistry.Registrar registrar) {
         if (registrar.activity() != null) {
             channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
             final FlutterWebviewPlugin instance = new FlutterWebviewPlugin(registrar.activity(), registrar.activeContext());
